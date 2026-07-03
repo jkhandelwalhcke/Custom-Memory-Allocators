83c55fe6db2c531e10b5433f6f719503c3613a4f
Jay Khandelwal <khandelwal14moriya@gmail.com>
2026-07-04 03:12:06 +0530

    Refactor multi-thread caching allocator implementation

--- Multi_thread_caching_Allocator/multi_thread_caching_allocator.c
+++ Multi_thread_caching_Allocator/multi_thread_caching_allocator.c
@@ -72,19 +72,13 @@ void refill_central_cache(int class_idx) {
     }
 
     char* mem_ptr = (char*)raw_mem;
-
     int num_blocks = chunk_size / block_size;
 
     for (int i = 0; i < num_blocks; i++) {
         
         char* current_addr = mem_ptr + (i * block_size);
-        
         struct Block* block = (struct Block*)current_addr;
-        
         block->next = central_cache[class_idx];
-        
         central_cache[class_idx] = block;
     }
 }
-
-
